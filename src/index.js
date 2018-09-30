ec1f1e8d15bacefe37d4b7e436273c08017c457b
stigmatory15 <repka_dmitriy@mail.ru>
2018-09-30 20:07:15 +0300

    task 7 complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let sum = 0;
+ while(number >= 5){
+   sum+=Math.floor(number / 5);
+   Math.floor(number = number / 5);
+ }
+ return sum;
 }
